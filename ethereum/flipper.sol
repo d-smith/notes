ffb80d97a7227d76beb15fb6fa4a45600230dbc2
Smith, Doug <doug.smith@fmr.com>
2018-05-01 06:13:02 -0700

    More ethereum notes

--- ethereum/flipper.sol
+++ ethereum/flipper.sol
@@ -3,23 +3,32 @@ contract Flipper {
   enum GameState{noWager, wagerMade, wagerAccepted}
 
   GameState public currentState;
+  uint public wager;
+  address public player1;
+  address public player2;
 
-  modifier onlyState(GameState expectedState) { if (expectedState == currentState) {_:} else {throw;}}
+  modifier onlyState(GameState expectedState) { if (expectedState == currentState) {_;} else {throw;}}
 
   function Flipper() {
     currentState = GameState.noWager;
   }
 
-  function makeWager() onlyState(GameState.noWager) returns (bool) {
-    // ...
+  function makeWager() onlyState(GameState.noWager) payable returns (bool) {
+    wager = msg.value;
+    player1 = msg.sender;
     currentState = GameState.wagerMade;
     return true;
   }
 
-  function acceptWager() onlyState(GameState.wagerMade) returns (bool) {
-    // ...
-    currentState = GameState.wagerAccepted;
-    return true;
+  function acceptWager() onlyState(GameState.wagerMade) payable returns (bool) {
+    if(msg.value == wager) {
+      player2 = msg.sender;
+      currentState = GameState.wagerAccepted;
+      return true;
+    } else {
+      throw;
+    }
+
   }
 
   function resolveBet() onlyState(GameState.wagerAccepted) returns (bool) {
