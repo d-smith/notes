3fa4113e364c0834508ef4ecd31bb74fbd7679b4
Smith, Doug <doug.smith@fmr.com>
2018-05-03 08:50:48 -0700

    Updates from part 11

--- ethereum/decipher.js
+++ ethereum/decipher.js
@@ -2,77 +2,80 @@
 global.config = {
   rpc: {
     host: "localhost",
-    port: "8545"
-  }
-}
+    port: "8545",
+  },
+};
 
 // Load Libraries
-global.solc = require("solc")
-global.fs = require("fs")
-global.Web3 = require("web3")
+global.solc = require("solc");
+global.fs = require("fs");
+global.Web3 = require("web3");
 
 // Connect Web3 Instance
-global.web3 = new Web3(new Web3.providers.HttpProvider(`http://${global.config.rpc.host}:${global.config.rpc.port}`))
+global.web3 = new Web3(new Web3.providers.HttpProvider(`http://${global.config.rpc.host}:${global.config.rpc.port}`));
 
 // Global Account Accessors
-global.acct1 = web3.eth.accounts[0]
-global.acct2 = web3.eth.accounts[1]
-global.acct3 = web3.eth.accounts[2]
-global.acct4 = web3.eth.accounts[3]
-global.acct5 = web3.eth.accounts[4]
+global.acct1 = web3.eth.accounts[0];
+global.acct2 = web3.eth.accounts[1];
+global.acct3 = web3.eth.accounts[2];
+global.acct4 = web3.eth.accounts[3];
+global.acct5 = web3.eth.accounts[4];
 
 // Helper Functions
 class Helpers {
 
   contractName(source) {
-    var re1 = /contract.*{/g
-    var re2 = /\s\w+\s/
-    return source.match(re1).pop().match(re2)[0].trim()
+    const re1 = /contract.*{/g;
+    const re2 = /\s\w+\s/;
+    return source.match(re1).pop().match(re2)[0].trim();
   }
 
   createContract(source, options={}) {
-    var compiled = solc.compile(source)
-    var contractName = ':' + this.contractName(source)
-    var bytecode = compiled["contracts"][contractName]["bytecode"]
-    var abi = JSON.parse(compiled["contracts"][contractName]["interface"])
-    var contract = global.web3.eth.contract(abi)
-    var gasEstimate = global.web3.eth.estimateGas({ data: bytecode })
-
-    var deployed = contract.new(Object.assign({
+    const compiled = solc.compile(source);
+    const contractName = this.contractName(source);
+    const contractToDeploy = compiled.contracts[`:${contractName}`];
+    const bytecode = contractToDeploy.bytecode;
+    const abi = JSON.parse(contractToDeploy.interface);
+    const contract = global.web3.eth.contract(abi);
+    const gasEstimate = global.web3.eth.estimateGas({ data: bytecode });
+    const contractTx = {
       from: global.web3.eth.accounts[0],
       data: bytecode,
       gas: gasEstimate,
-      gasPrice: 5
-    }, options), (error, result) => { })
-
-    return deployed
+      gasPrice: 5,
+      value: web3.toWei(5, 'ether')
+    };
+    const deployed = contract.new(Object.assign(
+      {},
+      contractTx,
+      options,
+    ), (error, result) => { });
+    return deployed;
   }
 
   loadContract(name) {
-    var path = `./${name.toLowerCase()}.sol`
-    return fs.readFileSync(path, 'utf8')
+    const path = `./${name.toLowerCase()}.sol`;
+    return fs.readFileSync(path, 'utf8');
   }
 
   deployContract(name, options={}) {
-    var source = this.loadContract(name)
-    return this.createContract(source, options)
+    const source = this.loadContract(name);
+    return this.createContract(source, options);
   }
 
   etherBalance(contract) {
     switch(typeof(contract)) {
       case "object":
         if(contract.address) {
-          return global.web3.fromWei(global.web3.eth.getBalance(contract.address), 'ether').toNumber()
+          return global.web3.fromWei(global.web3.eth.getBalance(contract.address), 'ether').toNumber();
         } else {
-          return new Error("cannot call getEtherBalance on an object that does not have a property 'address'")
+          return new Error("cannot call getEtherBalance on an object that does not have a property 'address'");
         }
         break
       case "string":
-        return global.web3.fromWei(global.web3.eth.getBalance(contract), 'ether').toNumber()
-        break
+        return global.web3.fromWei(global.web3.eth.getBalance(contract), 'ether').toNumber();
     }
   }
-
 }
 
 // Load Helpers into Decypher namespace
