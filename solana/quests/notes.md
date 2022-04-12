d141cefcec5ae6d4355c674ea4c36426ca0cc051
d-smith <doug.smith.mail@gmail.com>
2022-04-12 10:13:50 -0700

    finished 1st quest

--- solana/quests/notes.md
+++ solana/quests/notes.md
@@ -19,3 +19,49 @@ Bootstrap a project
 * entry file is main.rs, 
 * macro - has a band at the end (e.g. println!)
 * to run, cd into hello-world folder and 'cargo run'
+
+### 4A Vars and Immutability
+
+let x = initial-value
+
+static typing
+
+Simple types
+
+integers i8, i 32 etc
+floats - f32, f64
+chars - single utf character
+boolean - bool, true or false
+strings
+
+Composite types
+
+tuples, destructuring
+arrays - zero based index
+arrays can be typed, and immutability also applies
+
+### 5 Control Flow
+
+if, else if, else
+
+loops
+
+loop, break
+
+while\<condition\> {}
+
+for in range, exclusinve, e.g. 1 less than end of range
+0..10 goes from 0 to 9
+
+Inclusive - add equals - 0 to 9 is 0..=9
+
+Match statement
+
+Note coverage must be exhaustive
+
+match x {
+    1 => printlln!()
+    2 => ...
+    _ => ... //default
+}
+
--- solana/quests/quest1/hello-world/src/main.rs
+++ solana/quests/quest1/hello-world/src/main.rs
@@ -1,3 +1,75 @@
 fn main() {
-    println!("Hello, world!");
+
+    // type inference
+    let x = "hello, world";
+    println!("{}",x);
+
+    // mutable
+    let mut y = 4;
+    println!("y is {}",y);
+    y = 5;
+    println!("y is {}",y);
+
+    // explicit typing types
+    let a:i32 = 2;
+    println!("a is {}", a);
+
+    // tuples
+    let b  = (1, 3.14, "xxx", 'x', true);
+    println!("b.0 is {}", b.0);
+
+    //destructuring
+    let (v1, v2, _,_, v3) = b;
+    println!("{} {} {}", v1, v2,v3);
+
+    //array initialized with a number of the same elements
+    let c:[i32;10] = [0;10];
+    println!("{:?}", c);
+
+    if true {
+        println!("yep");
+    } else {
+        println!("nope");
+    }
+
+    let mut d = 1;
+    loop {
+        if d > 1000 {
+            break;
+        }
+
+        d *= 2;
+        println!("{}", d);
+    }
+
+    d = 1;
+    while d < 1000 {
+        d *= 2;
+        println!("{}", d);
+    }
+
+    // Exclusive range
+    for i in 0..10 {
+        println!("{}", i)
+    }
+
+    //Exclusive range
+    for i in 0..=9 {
+        println!("{}", i)
+    }
+
+    //Iterators
+    for val in c {
+        println!("{}", val)
+    }
+
+    //Matching
+    let x  = true;
+    let y = true;
+
+    match (x,y) {
+        (true,true) => println!( "x & y is true"),
+        (false,_) => println!("x & y is false"),
+        (_,false) => println!("x & y is false"),
+    }
 }
